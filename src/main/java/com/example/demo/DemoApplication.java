6ca8eab4f913c4383c649c574b4675629182f3d6
Noel Lim <noellimx@gmail.com>
2022-09-08 04:59:43 +0800

    add test framework

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Bean;
 
 public class DemoApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
