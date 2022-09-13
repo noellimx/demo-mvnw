eb03c4778b3c6f60092e5d2ce6b91f8012dcfe6f
Noel Lim <noellimx@gmail.com>
2022-09-14 01:40:50 +0800

    -

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private ControllerSome controller;
+
 	@Test
 	void contextLoads() {
-	}
 
+		assertThat(controller).isNotNull();
+
+	}
 }
